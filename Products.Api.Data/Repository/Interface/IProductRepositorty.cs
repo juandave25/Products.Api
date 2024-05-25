dbba8f2942e922c5cdc4b33a689a354f14bfd44d
Juan David Velez H <juandavidv4@gmail.com>
2024-05-25 16:01:33 -0500

    Add project files.

---
+++ Products.Api.Data/Repository/Interface/IProductRepositorty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Api.Data.Repository.Interface
+{
+    internal interface IProductRepositorty
+    {
+    }
+}
---
+++ Products.Api.Data/Repository/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Api.Data.Repository
+{
+    internal class ProductRepository
+    {
+    }
+}
